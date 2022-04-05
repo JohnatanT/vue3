889551f6198dff17b89be315f73bc2ac0d5ce99b
Cavalcante <francisco.cavalcante@capgemini.com>
2022-04-05 13:46:07 -0300

    first commit

---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Meu nome é: {{nome}}</h1>
+</template>
+
+<script>
+export default {
+    name: 'LifeCycle',
+    data() {
+        return {
+            nome: 'Ainda não sei'
+        }
+    },
+    created() {
+        setTimeout(() => {
+            this.nome = 'Johnatan'
+        }, 1000)
+        
+    },
+    mounted() {
+        setTimeout(() => {
+            this.nome = 'Johnatan2'
+        }, 2000)
+    },
+}
+</script>
